66d9f5db3b5847652f78587e43b0f4fdeca63cfe
gscanlon <git@ascallion.com>
2023-12-24 13:17:41 -0600

    fix: chart upload

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -13,3 +13,25 @@ $("[data-dismiss]").each((i, elem) => elem.addEventListener('click', (e) => {
     parent.remove();
     grandParent.cleanWhitespace();
 }))
+
+window.uploadChart = function uploadChart(chart, width, height, email, token, type) {
+    const offscreen = new OffscreenCanvas(width, height);
+    const chartDuplicate = new Chart(offscreen, chart.config);
+    setTimeout(async () => {
+        const blob = await offscreen.convertToBlob();
+        const form = new FormData();
+        const request = new XMLHttpRequest();
+        form.append("image", blob);
+        form.append("type", type);
+        form.append("email", email);
+        form.append("token", token);
+        request.open("POST", "/api/User/UploadImage", true);
+        request.send(form);
+        
+        //const reader = new FileReader()
+        //reader.onload = (e) => {
+        //    console.debug({ reader.result })
+        //}
+        //reader.readAsDataURL(blob) 
+    }, 1000);
+}
