4652b1e1e779ff85dfc5c78210007de4b9b9d811
gscanlon <git@ascallion.com>
2024-08-18 12:33:36 -0500

    improvement: sections

--- Core/Models/Newsletter/Section.cs
+++ Core/Models/Newsletter/Section.cs
@@ -9,17 +9,17 @@ public enum Section
     [Display(Name = "Anytime")]
     None = 0,
 
+    [Display(Name = "After Waking Up")]
+    AfterWakingUp = 1 << 0, // 1
+
     [Display(Name = "Before Breakfast")]
-    BeforeBreakfast = 1 << 0, // 1
+    BeforeBreakfast = 1 << 1, // 2
 
     [Display(Name = "With Breakfast")]
-    WithBreakfast = 1 << 1, // 2
+    WithBreakfast = 1 << 2, // 4
 
     [Display(Name = "After Breakfast")]
-    AfterBreakfast = 1 << 2, // 4
-
-    [Display(Name = "Morning")]
-    Morning = 1 << 3, // 8
+    AfterBreakfast = 1 << 3, // 8
 
     [Display(Name = "Before Lunch")]
     BeforeLunch = 1 << 4, // 16
@@ -30,45 +30,40 @@ public enum Section
     [Display(Name = "After Lunch")]
     AfterLunch = 1 << 6, // 64
 
-    [Display(Name = "Midday")]
-    Midday = 1 << 7, // 128
-
-    [Display(Name = "Afternoon")]
-    Afternoon = 1 << 8, // 256
-
     [Display(Name = "Before Dinner")]
-    BeforeDinner = 1 << 9, // 512
+    BeforeDinner = 1 << 7, // 128
 
     [Display(Name = "With Dinner")]
-    WithDinner = 1 << 10, // 1024
+    WithDinner = 1 << 8, // 256
 
     [Display(Name = "After Dinner")]
-    AfterDinner = 1 << 11, // 2048
-
-    [Display(Name = "Evening")]
-    Evening = 1 << 12, // 4096
-
-    [Display(Name = "Before Bed")]
-    BeforeBed = 1 << 13, // 8192
+    AfterDinner = 1 << 9, // 512
 
     [Display(Name = "Pre-Workout")]
-    PreWorkout = 1 << 14, // 16384
+    PreWorkout = 1 << 10, // 1024
 
     [Display(Name = "Post-Workout")]
-    PostWorkout = 1 << 15, // 32768
+    PostWorkout = 1 << 11, // 2048
 
-    All = Morning | Midday | Afternoon | PreWorkout | PostWorkout | BeforeBed,
+    [Display(Name = "Before Bed")]
+    BeforeBed = 1 << 12, // 4096
+
+    All = AfterWakingUp | BeforeBreakfast | WithBreakfast | AfterBreakfast 
+        | BeforeLunch | WithLunch | AfterLunch 
+        | BeforeDinner | WithDinner | AfterDinner 
+        | PreWorkout | PostWorkout | BeforeBed,
 }
 
 public static class SectionExtensions
 {
     public static ExerciseTheme AsTheme(this Section section) => section switch
     {
-        Section.Morning or Section.BeforeBreakfast or Section.WithBreakfast or Section.AfterBreakfast => ExerciseTheme.Other,
-        Section.Midday or Section.BeforeLunch or Section.WithLunch or Section.AfterLunch or Section.Afternoon => ExerciseTheme.Extra,
-        Section.Evening or Section.BeforeDinner or Section.WithDinner or Section.AfterDinner => ExerciseTheme.Cooldown,
-        Section.BeforeBed or Section.PreWorkout or Section.PostWorkout => ExerciseTheme.Main,
-        Section.None => ExerciseTheme.Warmup,
+        Section.AfterWakingUp => ExerciseTheme.Warmup,
+        Section.BeforeBreakfast or Section.WithBreakfast or Section.AfterBreakfast => ExerciseTheme.Other,
+        Section.BeforeLunch or Section.WithLunch or Section.AfterLunch => ExerciseTheme.Extra,
+        Section.BeforeDinner or Section.WithDinner or Section.AfterDinner => ExerciseTheme.Cooldown,
+        Section.PreWorkout or Section.PostWorkout or Section.BeforeBed => ExerciseTheme.Main,
+        Section.None => ExerciseTheme.None,
         _ => ExerciseTheme.None,
     };
 }
