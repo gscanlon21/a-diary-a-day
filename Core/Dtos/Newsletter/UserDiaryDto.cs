c86eb0047eaa6916b8627c7b3bd395e88344b890
gscanlon <git@ascallion.com>
2024-08-24 09:03:54 -0500

    chore: spelling

--- Core/Dtos/Newsletter/UserDiaryDto.cs
+++ Core/Dtos/Newsletter/UserDiaryDto.cs
@@ -4,20 +4,20 @@ public class UserDiaryDto
 {
     public int Id { get; init; }
 
-    public string Title()
-    {
-        return Date.ToLongDateString();
-    }
-
-    public string Description()
-    {
-        return $"{Date}";
-    }
-
     public int UserId { get; init; }
 
     /// <summary>
     /// The date the newsletter was sent out on
     /// </summary>
     public DateOnly Date { get; init; }
+
+    /// <summary>
+    /// Title to display for the list item in the app.
+    /// </summary>
+    public string Title() => Date.ToLongDateString();
+
+    /// <summary>
+    /// Description to display for the list item in the app.
+    /// </summary>
+    public string Description() => $"{Date}";
 }
