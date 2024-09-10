5342df4eaad292905620aef0bc5635044befe088
gscanlon <git@ascallion.com>
2024-09-10 13:09:43 -0500

    feat: backup of tasks

---
+++ Web/Controllers/User/UserController.BackupRestore.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Views.User;
+
+namespace Web.Controllers.User;
+
+public partial class UserController
+{
+    /// <summary>
+    /// User backup of task data.
+    /// </summary>
+    [HttpPost]
+    [Route("b", Order = 1)]
+    [Route("backup", Order = 2)]
+    public async Task<IActionResult> Backup(string email, string token)
+    {
+        var user = await _userRepo.GetUser(email, token);
+        if (user == null)
+        {
+            return View("StatusMessage", new StatusMessageViewModel(LinkExpiredMessage));
+        }
+
+        var tasks = await _context.UserTasks.Where(r => r.UserId == user.Id).ToListAsync();
+        return Json(new { tasks });
+    }
+}
